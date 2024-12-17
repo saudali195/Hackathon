1f8dffba14581b7c78373dfeec786aca858e691e
saudali195 <160694354+saudali195@users.noreply.github.com>
2024-12-18 02:17:03 +0500

    Hero.tsx

--- src/app/Components/Hero.tsx
+++ src/app/Components/Hero.tsx
@@ -246,7 +246,7 @@ const HeroSection = () => {
                 </div>
               </div>
               <div className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg transition duration-300">
-              <img src="/02.png" alt="Luxury Steel Chair" className="w-312 h-312 object-cover rounded-lg" />
+              <img src="/20.png" alt="Luxury Steel Chair" className="w-312 h-312 object-cover rounded-lg" />
               <div className="mt-4 text-center">
                 <h3 className="text-lg font-semibold text-[#272343]">Liabrary Stool Chair</h3>
                 <p className="text-teal-600 mt-2">$20</p>
